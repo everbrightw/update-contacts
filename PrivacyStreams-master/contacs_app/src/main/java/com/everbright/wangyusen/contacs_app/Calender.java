82e7789dc0eb0827a8f46853dcc708674f8c580b
yusenwang <yusenw2@illinois.edu>
2017-07-03 01:05:20 +0800

    add events and edit events
    add events and edit events

--- PrivacyStreams-master/contacs_app/src/main/java/com/everbright/wangyusen/contacs_app/Calender.java
+++ PrivacyStreams-master/contacs_app/src/main/java/com/everbright/wangyusen/contacs_app/Calender.java
@@ -1,5 +1,6 @@
 package com.everbright.wangyusen.contacs_app;
 
+import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
@@ -12,14 +13,8 @@ import com.github.privacystreams.calendar.CalendarEvent;
 import com.github.privacystreams.core.UQI;
 import com.github.privacystreams.core.exceptions.PSException;
 import com.github.privacystreams.core.purposes.Purpose;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
 
-import static android.support.v4.content.ContextCompat.startActivity;
 
 /**
  * Created by wangyusen on 7/1/17.
@@ -36,6 +31,11 @@ public class Calender {
      */
     private UQI uqi;
     private Purpose purpose;
+    Calender(Context context){
+        this.purpose = Purpose.TEST("test");
+        this.uqi = new UQI(context);
+    }
+
     public static Intent addCalendar_event(int year, int month, int day, int time, int endtime, String event_title,
                                     String description, String location, String email){
         Calendar beginTime = Calendar.getInstance();
@@ -56,6 +56,7 @@ public class Calender {
     /*
      *@overload method with only event_time and event title
      */
+
     public static Intent addCalendar_event(int year, int month, int day, int time, int endtime, String event_title
                                    ){
         Calendar beginTime = Calendar.getInstance();
@@ -70,13 +71,20 @@ public class Calender {
 
         return intent;
     }
-    public int getCalendareventID( String original_title) throws PSException {
-        int entryID =
-                uqi.getData(CalendarEvent.getAll(), purpose).filter("title", original_title).getFirst().getField("title");
-
+    public String getCalendareventID( String original_title) throws PSException {
+        String entryID =
+                uqi.getData(CalendarEvent.getAll(), purpose).filter("title", original_title).getItemAt(0).getField("id");
         return entryID;
 
+    }
+    public static Intent UpdateCalendarEntry() {
+        long eventID = 1503;
 
+        Uri uri = ContentUris.withAppendedId(CalendarContract.Events.CONTENT_URI, eventID);
+        Intent intent = new Intent(Intent.ACTION_EDIT)
+                .setData(uri)
+                .putExtra(CalendarContract.Events.TITLE,"new_title");
+        return intent;
 
     }
 
--- PrivacyStreams-master/contacs_app/src/main/java/com/everbright/wangyusen/contacs_app/MainActivity.java
+++ PrivacyStreams-master/contacs_app/src/main/java/com/everbright/wangyusen/contacs_app/MainActivity.java
@@ -1,12 +1,20 @@
 package com.everbright.wangyusen.contacs_app;
 
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.OperationApplicationException;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.RemoteException;
 import android.provider.CalendarContract;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import android.widget.TextView;
@@ -20,32 +28,37 @@ import static android.support.v4.content.ContextCompat.startActivity;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-        /** Called when the user taps the Send button */
-        public void sendMessage(View view) throws PSException, RemoteException, OperationApplicationException {
-            TextView textView = (TextView) findViewById(R.id.textView);
-            Contacts s = new Contacts(getApplicationContext());
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) throws PSException, RemoteException, OperationApplicationException {
+        TextView textView = (TextView) findViewById(R.id.textView);
+        Contacts s = new Contacts(getApplicationContext());
 
-            s.getContacts_name();
-            s.getContacts_id();
+        s.getContacts_name();
+        s.getContacts_id();
 
-            s.updateContactsEmail(getApplicationContext(), "6", "success@");
-            s.updateContactsPhone(getApplicationContext(),"6", "13613215058");
-            textView.setText(s.getDisplayName()+s.getDisplayID()  );
+        s.updateContactsEmail(getApplicationContext(), "6", "success@");
+        s.updateContactsPhone(getApplicationContext(), "6", "13613215058");
+        textView.setText(s.getDisplayName() + s.getDisplayID());
 
+    }
 
+    public void testCalender(View view) throws PSException {
+        TextView textView = (TextView) findViewById(R.id.textView);
 
-        }
-        public void testCalender(View view) throws PSException {
-            TextView textView = (TextView) findViewById(R.id.textView);
+        Calender a = new Calender(getApplicationContext());
+        textView.setText(a.getCalendareventID("omg"));
+        //  a.UpdateCalendarEntry(getContentResolver());
+        //startActivity(Calender.addCalendar_event(2017,7,1,730,830,"wow"));
 
+        startActivity(Calender.UpdateCalendarEntry());
 
-            startActivity(Calender.addCalendar_event(2017,7,1,730,830,"wow"));
         }
 
 
